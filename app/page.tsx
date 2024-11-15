e2eb64bcde6fca695f2e7b1b75d8d1409c75463f
Zach Price <37559274+zachchan105@users.noreply.github.com>
2024-11-15 13:42:36 -0800

    minor link change

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const socialLinks = [
   { name: "Twitter", icon: FaTwitter, href: "https://x.com/GuacSwapKRC20", color: "bg-blue-400" },
   { name: "Telegram", icon: FaTelegramPlane, href: "https://t.me/guac_token", color: "bg-blue-500" },
   { name: "Discord", icon: FaDiscord, href: "https://discord.gg/d2QzKP5r8j", color: "bg-indigo-600" },
-  { name: "GUAC Swap", icon: ArrowRightLeft, href: "https://swap.guac.fyi/token/guac", color: "bg-green-500" },
+  { name: "GUAC Swap", icon: ArrowRightLeft, href: "https://swap.guac.fyi/", color: "bg-green-500" },
 ]
 
 const tokens = [
